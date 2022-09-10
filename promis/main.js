4aded29043fdff181b4278520877790a9da54036
kernel_loophole <p190119@nu.edu.pk>
2022-09-10 09:41:56 -0400

    promise

---
+++ promis/main.js
@@ -0,0 +1,33 @@
+function check_grade_succes(result)
+{
+    console.log("create function ",result);
+}
+function check_grade_fail(result)
+{
+    console.log("fail to create ");
+}
+//simple way 
+function check_result()
+{
+    console.log("print");
+}
+
+check_result(setting,check_grade_fail,check_grade_succes);
+//using promise
+check_result(setting).then(check_grade_fail,check_grade_succes);
+
+function doSomething()
+{
+    console.log("current");
+}
+
+doSomething().then(function (result) {
+    return doSomethingElse(result);
+  })
+  .then(function (newResult) {
+    return doThirdThing(newResult);
+  })
+  .then(function (finalResult) {
+    console.log(`Got the final result: ${finalResult}`);
+  })
+  .catch(failureCallback);
